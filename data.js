0729405ea81916642cc33d1e55458b9423933b9a
armaansahiba2916-sys <armaansahiba2916@gmail.com>
2026-02-16 00:08:16 -0500

    Add comic data to data.js

--- data.js
+++ data.js
@@ -1,1 +1,19 @@
+const COMICS = [
+  {
+    id: 1,
+    title: "Test Image ( Gojo )",
+    img: "comics/c68.jpg",
+    alt: "Nah, I'd Win",
+    hover: "But would you lose? 'Nah, I'd win' ",
+    date: "2026-02-15"
+  },
+  {
+    id: 2,
+    title: "Test 2",
+    img: "comics/391f8aab-d6f6-4d66-95db-d7d4befd9964.png",
+    alt: "Lucas",
+    hover: "That feeling when you have a sleepover tommorow.",
+    date: "2026-02-15"
+  }
+];
 
