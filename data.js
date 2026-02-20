99d5cb1568595e1317ec94880c3e160bbacff410
Sketchesandstuff <armaansahiba2916@gmail.com>
2026-02-20 17:20:04 -0500

    Update comic entries in data.js
    Replaced the first comic entry with a new random sketch and removed the second comic entry.

--- data.js
+++ data.js
@@ -1,19 +1,11 @@
 const COMICS = [
   {
     id: 1,
-    title: "Test Image ( Gojo )",
-    img: "comics/c68.jpg",
-    alt: "Nah, I'd Win",
-    hover: "But would you lose? 'Nah, I'd win' ",
+    title: "Random Sketch",
+    img: "comics/IMG_20260219_142843.jpg",
+    alt: "Random sketch",
+    hover: "IDK, but Kagami's face looks kinda weird.",
     date: "2026-02-15"
   },
-  {
-    id: 2,
-    title: "Test 2",
-    img: "comics/391f8aab-d6f6-4d66-95db-d7d4befd9964.png",
-    alt: "Lucas",
-    hover: "That feeling when you have a sleepover tommorow.",
-    date: "2026-02-15"
-  }
 ];
 
